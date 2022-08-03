8bb484691a13f8f0af341132ac4ac85ea4c086bd
Sachin <bandalsachin4747@gmail.com>
2022-08-03 21:16:41 +0530

    new pojo class Ticket added with id from

---
+++ src/main/java/com/bookticket/TodayRailways0022/entity/Ticket.java
@@ -0,0 +1,20 @@
+package com.bookticket.TodayRailways0022.entity;
+
+public class Ticket {
+	int id;
+	String from;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	
+
+}
